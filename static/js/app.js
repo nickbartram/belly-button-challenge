26432209634679511c2a26d94921d2ed2018654a
nickbartram <nickbartram25@gmail.com>
2024-10-03 16:12:46 -0400

    Very rough first draft complete: dropdown, bar, bubbble work - Demographic info does not work. Likely a lot of other errors

--- static/js/app.js
+++ static/js/app.js
@@ -20,8 +20,9 @@ function buildMetadata(sample) {
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-    for 
-
+    Object.entries(filterData[0]).forEach(([key, value]) => {
+      panel.append("h6").text(`${key}: ${value}`);
+    });
   });
 }
 
@@ -30,48 +31,62 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-    let sample = data.samples;
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
     const sampleID = obj => obj.id === sample;
     
-    let filterSample = samples.filter(sampleID);
+    let filterSample = samples.filter(sampleID)[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-    let otu_ids = sample.samples.otu_ids;
-    let otu_labels = sample.samples.otu_labels;
-    let sample_values = sample.samples.sample_values;
+    let otu_ids = filterSample.otu_ids;
+    let otu_labels = filterSample.otu_labels;
+    let sample_values = filterSample.sample_values;
 
     // Build a Bubble Chart
     let trace1 = {
       x: otu_ids,
       y: sample_values,
-      colors: otu_ids,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids
+      },
       text: otu_labels
     };
 
-    let data = [trace1];
+    let data1 = [trace1];
 
     let layout = {
       title: "Test"
     };
   
-
-    Plotly.newPlot("plot", data, layout);
-  
     // Render the Bubble Chart
-    buildCharts(sample);
+    Plotly.newPlot("bubble", data1, layout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let otuidNames = [otu_ids];
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    let trace2 = {
+      x: sample_values.slice(0, 10).reverse(),
+      y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+      type: "bar",
+      orientation: "h"
+    }
+
+    let data2 = [trace2];
+
+    let layout2 = {
+      title: "Top 10 OTU"
+    };
 
 
     // Render the Bar Chart
-
- 
+    Plotly.newPlot("bar", data2, trace2);
+  });
+}
 
 
 // Function to run on page load
@@ -82,25 +97,30 @@ function init() {
 
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    data.names.forEach((sample) => {
+      dropdown.append("option").text(sample).property("value", sample);
+    });
 
     // Get the first sample from the list
-
+    let firstSample = data.names[0];
 
     // Build charts and metadata panel with the first sample
-
+    
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialize the dashboard
